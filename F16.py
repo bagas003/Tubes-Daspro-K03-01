16532e065af24f49d47543001a3c032674775aea
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-10 14:42:48 +0700

    Add files via upload

---
+++ F16.py
@@ -0,0 +1,27 @@
+def save(user,game,riwayat,kepemilikan):
+    
+    def array_to_string(arr):
+        converted_data = ''
+        for line in arr:
+            for elmt in line:
+                converted_data += str(elmt) + ';'
+            converted_data += '\n'
+        return converted_data
+
+    import os
+
+    dir = input('Masukkan nama folder penyimpanan: ')
+
+    if os.path.isdir(dir):
+        os.rmdir(dir)
+
+    os.makedirs(dir)
+
+    var_pointer = [user, game, riwayat, kepemilikan]
+    str_pointer = ['user', 'game', 'riwayat', 'kepemilikan']
+    for i in range(4):
+        f = open(fr'{dir}\{str_pointer[i]}', 'w')
+        f.write(array_to_string(var_pointer[i]))
+        f.close()
+
+
