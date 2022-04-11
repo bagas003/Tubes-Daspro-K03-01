50ea7b02ea1367c6ee9e3d264dc42c700a64bd45
Albert <88728615+albert4183r7@users.noreply.github.com>
2022-04-11 11:28:01 +0700

    Update F13.py

--- F13.py
+++ F13.py
@@ -1,5 +1,5 @@
 # F13 - Melihat Riwayat Pembelian
-def riwayat():
+def riwayat(data_history):
     # menampilkan daftar riwayat pembelian game 
     # I.S. pengguna sudah login dan matriks data file riwayat terdefinisi
     # F.S. database riwayat pembelian game tercetak ke layar
@@ -13,36 +13,12 @@ def riwayat():
 
     isHistEmpty = True
     print("Daftar game:")
-    for i in range(1,len(history)):
-        if (history[i][3] == input_user_id):
+    for i in range(1,len(data_history)):
+        if (data_history[i][3] == input_user_id):
             isHistEmpty = False
-            print('{:<6} | {:<29} | {:<7} | {:<6}'.format(history[i][0],history[i][1],history[i][2],history[i][4]))
+            print('{:<6} | {:<29} | {:<7} | {:<6}'.format(data_history[i][0],data_history[i][1],data_history[i][2],data_history[i][4]))
     if isHistEmpty:
         print("Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah beli_game untuk membeli.")
+    return data_history
 
 # referensi : https://www.semicolonworld.com/question/44245/python-format-output-string-right-alignment?msclkid=0e4d8a87b89911ecb9c76c8b7a60bae6
-
-# PROGRAM UTAMA
-
-# KAMUS GLOBAL
-# type data_history : <game_id: string, nama: string, harga: int, user_id: string, tahun_beli: int>
-# history : array of data_history
-# isProgramValid : boolean
-# menu : string
-
-# INISIALISASI
-# Note : data dibawah ini hanya digunakan untuk mengetes jalannya program
-# ketika nanti digabung, abaikan saja.
-history = [['game_id','nama','harga','user_id','tahun_beli'],
-        ['GAME001','BNMO - Play Along With Crypto',100000,1,2022],
-        ['GAME002','Python Gemink',100000,2,2001],
-        ['GAME003','Hehehe',100000,3,2021]]
-
-# ALGORITMA
-isProgramValid = True
-while isProgramValid:
-    menu = input()
-    if menu == "riwayat":
-        riwayat()
-    else:
-        exit()
