3184cc1cec4dc00a56fba56ad3d20157887515d0
Albert <88728615+albert4183r7@users.noreply.github.com>
2022-04-11 11:22:26 +0700

    Update F09.py

--- F09.py
+++ F09.py
@@ -1,17 +1,17 @@
 # F09 - Melihat Game yang dimiliki
-def listgame(game):
+def listgame(data_game):
     # menampilkan daftar game yang dimiliki pengguna ke layar
-    # I.S. pengguna sudah login dan matriks data file game terdefinisi
+    # I.S. pengguna sudah login dan matriks data file data_game terdefinisi
     # F.S. database game tercetak ke layar
 
     # KAMUS LOKAL
 
     # ALGORITMA
-    if len(game) == 1:
+    if len(data_game) == 1:
         print("Maaf, kamu belum membeli game. Ketik perintah beli_game untuk beli.")
     else: 
         print("Daftar game:")
-        for i in range(1,len(game)):
-            print('{:<6} | {:<29} | {:<9} | {:<4} | {:<6}'.format(*game[i]))
-    return game
+        for i in range(1,len(data_game)):
+            print('{:<6} | {:<29} | {:<9} | {:<4} | {:<6}'.format(*data_game[i]))
+    return data_game
 # referensi : https://www.semicolonworld.com/question/44245/python-format-output-string-right-alignment?msclkid=0e4d8a87b89911ecb9c76c8b7a60bae6
