0836ad225d0f1924dba372d534e5b67cadfb3fe6
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-12 15:34:26 +0700

    Update F08.py

--- F08.py
+++ F08.py
@@ -6,16 +6,16 @@ def beli_game(id_user, data_user, data_game, data_riwayat, data_kepemilikan):
 
     if int(data_game[index_game][4]) > int(data_user[index_user][5]):
         print('Saldo anda tidak cukup untuk membeli Game tersebut!')
-        return
+        return data_user, data_game, data_riwayat, data_kepemilikan
 
     for data in data_kepemilikan:
         if data[0] == id_game and data[1] == id_user:
             print('Anda sudah memiliki Game tersebut!')
-            return
+            return data_user, data_game, data_riwayat, data_kepemilikan
     
     if int(data_game[index_game][5]) < 1:
         print('Stok Game tersebut sedang habis!')
-        return
+        return data_user, data_game, data_riwayat, data_kepemilikan
     
     data_user[index_user][5] = str(int(data_user[index_user][5]) - int(data_game[index_game][4]))
     data_game[index_game][5] = str(int(data_game[index_game][5]) - 1)
