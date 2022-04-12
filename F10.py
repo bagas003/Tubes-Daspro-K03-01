1068082c2debf7760a49f7565f54fc2bcc594c19
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-12 15:39:40 +0700

    Update F10.py

--- F10.py
+++ F10.py
@@ -17,15 +17,21 @@ def buat_kepemilikan(user_id, data_kepemilikan, data_game):
     temp += _cari_data(game[0], 0, data_game)
   return temp
 
-def search_my_game(data_game_user):
+def search_my_game(user_id, data_kepemilikan, data_game):
   # Akses : user
   # Menerima input data game dari sebuah user lalu mencari game dengan id dan input yang
   # dimasukkan lalu menampilkannya ke layar
-  id = input("Masukkan ID Game: ")
-  tahun = int(input("Masukkan Tahun Rilis Game: "))
 
-  if id == '':
+  data_game_user = buat_kepemilikan(user_id, data_kepemilikan, data_game)
+  id = input("Masukkan ID Game: ")
+  tahun = input("Masukkan Tahun Rilis Game: ")
+
+  if id == tahun == '':                                                    # ini pengecekan ditambahi kalo keduanya kosong atau tahunnya doang yg kosong
+    data_hasil = data_game_user
+  elif id == '':
     data_hasil = _cari_data(tahun, 3, data_game_user)
+  elif tahun == '':
+    data_hasil = _cari_data(id, 0, data_game_user)
   else:
     data_hasil = _cari_data(id, 0, _cari_data(tahun, 3, data_game_user))
 
@@ -35,5 +41,5 @@ def search_my_game(data_game_user):
   else:
     i = 1
     for game in data_hasil:
-      print(f"{i}. {game[0]:5s} | {game[1]:20s} | {game[4]:7d} | {game[2]:10s} | {game[3]:4d}")
+      if game[0] != 'id': print(f"{i}. {game[0]:5s} | {game[1]:20s} | {game[4]:7s} | {game[2]:10s} | {game[3]:4s}")
       i += 1
