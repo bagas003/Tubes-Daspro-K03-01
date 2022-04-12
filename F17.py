dd39fdbcfaba8f627fdb64d56e7ba191c345f999
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-12 15:49:12 +0700

    Update F17.py

--- F17.py
+++ F17.py
@@ -1,10 +1,11 @@
 def exit():
     simpan = input('Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ')
 
-    while simpan != 'Y' or 'y' or 'N' or 'n':
+    while simpan not in ('y', 'Y', 'n', 'N'):
         simpan = input('Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ')
 
-    if simpan == 'Y' or 'y': return True
-    else: return False
+    if simpan in ('y', 'Y'): return True
+    
+    return False
     
     
