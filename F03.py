937c98e557b1b661021b4a7ef91cf397b86ef241
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-12 15:10:02 +0700

    Update F03.py

--- F03.py
+++ F03.py
@@ -4,14 +4,14 @@ def login(data):
   # login berhasil, serta menghasilkan status apakah login berhasil
   username = input("Masukan username: ")
   password = input("Masukan password: ")
-  isLoginBerhasil = False
 
   for user in data:
     if user[1] == username and user[3] == password:
       print(f'Halo {user[1]}! Selamat datang di "Binomo".')
       isAdmin = user[4] == 'admin'
       isLoginBerhasil = True
-      return isAdmin, user[0], isLoginBerhasil
+      return isAdmin, user[0]
   
   print("Password atau username salah atau tidak ditemukan.")
-  return None, None, isLoginBerhasil
+  return login(data)
+
