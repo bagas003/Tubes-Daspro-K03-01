66bf522f28e59151d040c57072556d51a9755079
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-12 15:43:54 +0700

    Update F13.py

--- F13.py
+++ F13.py
@@ -1,5 +1,5 @@
 # F13 - Melihat Riwayat Pembelian
-def riwayat(data_history):
+def riwayat(user_id, history):                                             # user_id dijadikan sebagai parameter dan bukan sbg input
     # menampilkan daftar riwayat pembelian game 
     # I.S. pengguna sudah login dan matriks data file riwayat terdefinisi
     # F.S. database riwayat pembelian game tercetak ke layar
@@ -9,11 +9,9 @@ def riwayat(data_history):
     # isHistEmpty : boolean
     
     # ALGORITMA
-    input_user_id = int(input("Masukan user_id: "))
-
     isHistEmpty = True
     for i in range(1,len(history)):
-        if (history[i][3] == input_user_id):
+        if (history[i][3] == user_id):
             if isHistEmpty: # aksi ini pasti dilakukan 1x
                 print("Daftar game:") 
             isHistEmpty = False
@@ -21,6 +19,3 @@ def riwayat(data_history):
             
     if isHistEmpty:
         print("Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah beli_game untuk membeli.")
-    return data_history
-
-# referensi : https://www.semicolonworld.com/question/44245/python-format-output-string-right-alignment?msclkid=0e4d8a87b89911ecb9c76c8b7a60bae6
