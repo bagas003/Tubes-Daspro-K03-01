119161410b0d5fc63345f6c4e5f89957ee10e51c
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-12 15:37:39 +0700

    Update F09.py

--- F09.py
+++ F09.py
@@ -1,5 +1,7 @@
 # F09 - Melihat Game yang dimiliki
-def listgame(data_kepemilikan,data_game):
+import tools
+
+def list_game(user_id, kepemilikan, game):
     # menampilkan daftar game yang dimiliki pengguna ke layar
     # I.S. pengguna sudah login dan matriks data file game dan kepemilikan terdefinisi
     # F.S. daftar game yang dimiliki user tercetak ke layar
@@ -9,17 +11,14 @@ def listgame(data_kepemilikan,data_game):
     # Found : boolean
     
     # ALGORITMA
-    input_user_id = int(input("Masukkan user_id: "))
     Found = False
     for i in range(1,len(kepemilikan)):
-        if (input_user_id == kepemilikan[i][1]):
-            if (kepemilikan[i][0] == game[i][0]):
-                if not Found: # Aksi ini pasti dilakukan 1x
-                    print("Daftar game:")
-                Found = True
-                print('{:<6} | {:<29} | {:<9} | {:<4} | {:<6}'.format(*game[i]))
+        if (user_id == kepemilikan[i][1]):
+            if not Found: # Aksi ini pasti dilakukan 1x
+                print("Daftar game:")
+            Found = True
+            print('{:<6} | {:<29} | {:<9} | {:<4} | {:<6}'.format(*game[tools.get_index(kepemilikan[i][0])]))
             
     if not Found:
         print("Maaf, kamu belum membeli game. Ketik perintah beli_game untuk beli.")
-    return data_kepemilikan
 # referensi : https://www.semicolonworld.com/question/44245/python-format-output-string-right-alignment?msclkid=0e4d8a87b89911ecb9c76c8b7a60bae6
