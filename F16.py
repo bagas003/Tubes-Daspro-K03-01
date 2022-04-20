f466aae90e03590e9bab7f93ec21cee536d6b0be
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-20 11:28:42 +0700

    Update F16.py

--- F16.py
+++ F16.py
@@ -3,15 +3,17 @@ import tools
 
 def save(user,game,riwayat,kepemilikan):
     dir = input('Masukkan nama folder penyimpanan: ')
+    print('\nsaving...')
 
     if os.path.isdir(dir):
-        os.rmdir(dir)
-
-    os.makedirs(dir)
+        os.replace(dir, dir)
+    else:
+        os.makedirs(dir)
 
     var_pointer = [user, game, riwayat, kepemilikan]
     str_pointer = ['user', 'game', 'riwayat', 'kepemilikan']
     for i in range(4):
-        f = open(fr'{dir}\{str_pointer[i]}', 'w')
+        f = open(fr'{dir}\{str_pointer[i]}.csv', 'w')
         f.write(tools.array_to_string(var_pointer[i]))
         f.close()
+    print('Data telah disimpan pada folder', dir)
