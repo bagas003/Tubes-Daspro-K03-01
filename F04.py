e31dc6ea2ffa64f3f5a90eadc7c0271bbaec96c6
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-20 11:07:05 +0700

    Update F04.py

--- F04.py
+++ F04.py
@@ -2,20 +2,20 @@ import tools
 
 def tambah_game(data_game):
     id = 'GAME' + f"{tools.get_index(data_game[-1][0]) + 1:03d}"
-    nama = input('\nnama game: ')
-    kategori = input('kategori: ')
-    tahun = input('tahun rilis: ')
-    harga = input('harga: ')
-    stok = input('stok awal: ')
+    nama = input('Masukkan nama game: ')
+    kategori = input('Masukkan kategori: ')
+    tahun = input('Masukkan tahun rilis: ')
+    harga = input('Masukkan harga: ')
+    stok = input('Masukkan stok awal: ')
     new_game_data = [id, nama, kategori, tahun, harga, stok]
 
     while not (nama and kategori and tahun and harga and stok):
         print('\nMohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.')
-        nama = input('\nnama game: ')
-        kategori = input('kategori: ')
-        tahun = input('tahun rilis: ')
-        harga = input('harga: ')
-        stok = input('stok awal: ')
+        nama = input('Masukkan nama game: ')
+        kategori = input('Masukkan kategori: ')
+        tahun = input('Masukkan tahun rilis: ')
+        harga = input('Masukkan harga: ')
+        stok = input('Masukkan stok awal: ')
         new_game_data = [id, nama, kategori, tahun, harga, stok]
 
     data_game += [new_game_data]
