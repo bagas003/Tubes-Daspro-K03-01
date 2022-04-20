de45ffb37cef35acf67274887a536dd187799e27
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-20 11:16:09 +0700

    Update F05.py

--- F05.py
+++ F05.py
@@ -1,20 +1,21 @@
 import tools
 
 def ubah_game(data_game):
-    id = input('ID game: ')
-    nama = input('nama game: ')
-    kategori = input('kategori: ')
-    tahun = input('tahun rilis: ')
-    harga = input('harga: ')
+    id = input('Masukkan ID game: ')
+    nama = input('Masukkan nama game: ')
+    kategori = input('Masukkan kategori: ')
+    tahun = input('Masukkan tahun rilis: ')
+    harga = input('Masukkan harga: ')
 
     id = tools.get_index(id)
     ubah_data = [nama, kategori, tahun, harga]
 
     if id < 1 or id > tools.get_index(data_game[-1][0]):
-        print('Tidak ada game dengan ID tersebut!')
+        print('\nTidak ada game dengan ID tersebut!')
     else:
         for i in range(4):
             if ubah_data[i] != '':
                 data_game[id][i+1] = ubah_data[i]
+        print('\nSelamat! Berhasil mengubah game', data_game[id][1])
 
     return data_game
