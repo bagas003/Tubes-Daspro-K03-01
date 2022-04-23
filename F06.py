f51daac4d567c0e3404cfd5cbc86dea63cbaee97
Albert <88728615+albert4183r7@users.noreply.github.com>
2022-04-23 21:00:07 +0700

    Update F06.py

--- F06.py
+++ F06.py
@@ -9,41 +9,40 @@ def ubah_stok(data_game):
     # KAMUS LOKAL
     # input_id : string
     # input_jumlah : int
-    # isjumlahValid, found : boolean
+    # isjumlahValid : bool
     # i : int 
     # IX : int (Indeks dimana id ditemukan)
 
     # ALGORITMA
     input_id = input("Masukkan ID game: ")
 
-    # Validasi input_jumlah
-    isjumlahValid = False
-    while not isjumlahValid:
-        input_jumlah = int(input("Masukkan jumlah: ")) 
-        if (input_jumlah == 0): # Asumsi jumlah yg diinput != 0
-            print ("Silahkan masukkan kembali angka yang benar ( min. 1 )")
-        else:
-            isjumlahValid = True
-            
-    found = False
+    # Validasi ID Game
     IX = -999
     if input_id[:4] == 'GAME':
         for i in range(1,tools.panjang(data_game)):
             if data_game[i][0] == input_id:
-                found = True
                 IX = i
                 break
-        
+
     if IX == -999:
         print ("Tidak ada game dengan ID tersebut!")
     else: # IX != -999
+        # Validasi input_jumlah
+        isjumlahValid = False
+        while not isjumlahValid:
+            input_jumlah = int(input("Masukkan jumlah: ")) 
+            if (input_jumlah == 0): # Asumsi jumlah yg diinput != 0
+                print ("Silahkan masukkan kembali angka yang benar ( min. 1 )")
+            else:
+                isjumlahValid = True
+                
         # Validasi stok game setelah pengubahan (tidak negatif)
         if ((int(data_game[IX][5]) + input_jumlah) < 0):
-            print("Stok game",data_game[IX][1],"gagal dikurangi karena stok kurang. Stok sekarang:",data_game[IX][5],"(<",str(abs(input_jumlah)) + ")")
+            print(f'Stok game {data_game[IX][1]} gagal dikurangi karena stok kurang. Stok sekarang: {data_game[IX][5]} (< {0-input_jumlah})')
         else: # (data_game[IX][5] + input_jumlah) >= 0
             data_game[IX][5] = str(int(data_game[IX][5]) + input_jumlah)
             if (input_jumlah < 0):
-                print ("Stok game",data_game[IX][1],"berhasil dikurangi. Stok sekarang:",data_game[IX][5])
+                print (f'Stok game {data_game[IX][1]} berhasil dikurangi. Stok sekarang: {data_game[IX][5]}')
             elif (input_jumlah > 0):
-                print ("Stok game",data_game[IX][1],"berhasil ditambahkan. Stok sekarang:",data_game[IX][5])
+                print (f'Stok game {data_game[IX][1]} berhasil ditambahkan. Stok sekarang: {data_game[IX][5]}')
     return data_game
