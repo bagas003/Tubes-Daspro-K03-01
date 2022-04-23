03219465162e8bba07d9364811f7f454916037e8
Albert <88728615+albert4183r7@users.noreply.github.com>
2022-04-23 16:43:01 +0700

    Ubah len

--- B01.py
+++ B01.py
@@ -1,15 +1,16 @@
 # Vigenere cipher
+import tools
 
 def _buat_key(pesan, key):
   hasil = key
-  for i in range(len(pesan) - len(key)):
-    hasil += key[i % len(key)]
+  for i in range(tools.panjang(pesan) - tools.panjang(key)):
+    hasil += key[i % tools.panjang(key)]
   return hasil
 
 def encrypt(pesan, key):
   key = _buat_key(pesan, key)
   hasil = ""
-  for i in range(len(pesan)):
+  for i in range(tools.panjang(pesan)):
     x = ord(pesan[i]) + ord(key[i]) - 66
     x = x % 93 + 33
     hasil += chr(x)
@@ -18,7 +19,7 @@ def encrypt(pesan, key):
 def decrypt(pesan, key):
   key = _buat_key(pesan, key)
   hasil = ""
-  for i in range(len(pesan)):
+  for i in range(tools.panjang(pesan)):
     x = ord(pesan[i]) - ord(key[i])
     x = x % 93 + 33
     hasil += chr(x)
