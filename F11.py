aeeffcd1a4fa7b32c0ab5489a5bcc97348828fa6
bagas003 <103223351+bagas003@users.noreply.github.com>
2022-04-23 13:47:26 +0700

    Update F11.py

--- F11.py
+++ F11.py
@@ -1,6 +1,19 @@
+# F11 - Mencari Game di Toko
 import tools
 
 def search_game_at_store(data_game):
+    # mencari game di toko dengan parameter id, nama, harga, kategori, dan tahun rilis
+    # I.S. input data game dari pengguna
+    # F.S. game yang sesuai dengan input pengguna
+
+    # KAMUS
+    # data_game, matched_games : array of Game
+    # array_input, data : Game
+    # ada, matched : bool
+    # i : int
+
+    # ALGORITMA UTAMA
+    # inisiasi dan input pengguna
     array_input = ['','','','','']
     array_input[0] = input('Masukkan ID Game: ')
     array_input[1] = input('Masukkan Nama Game: ')
@@ -11,6 +24,7 @@ def search_game_at_store(data_game):
     ada = False
     matched_games = []
 
+    # mengecek game yang cocok dan memasukkannya ke matched_games
     print('\nDaftar game pada toko yang memenuhi kriteria:')
     for data in data_game:
         if data[0] != 'id':
@@ -22,5 +36,8 @@ def search_game_at_store(data_game):
                 ada = True
                 matched_games += [data]
     
-    if ada: tools.print_data(matched_games)
-    else: print('Tidak ada game pada inventory-mu yang memenuhi kriteria')
+    # menampilkan pesan atau data game yang cocok
+    if ada: 
+        tools.print_data(matched_games)
+    else: 
+        print('Tidak ada game pada inventory-mu yang memenuhi kriteria')
